e0beeeb8eea6ce7303044de683f12702f462ce7f
robert5066 <robert@gmail.com>
2017-04-23 15:31:47 +0800

    加入 错误码

--- app/client/__init__.py
+++ app/client/__init__.py
@@ -3,6 +3,7 @@ client = Blueprint("client",__name__)
 
 client_v1_0 = Blueprint("client_v1_0",__name__)
 client_v1_1 = Blueprint("client_v1_1",__name__)
+
 from . import views
 from . import views_v1_0
 from . import views_v1_1
